adb98e6f46e6d88abae16ec1134001a3d6b848b5
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-21 16:41:45 +0300

    Step 4

--- index.js
+++ index.js
@@ -13,11 +13,27 @@ check the local host: http://localhost:${port}`))
 // test route
 app.get('/test' , (request, response) => {
     response.json({status: 200 , message : "OK"})
-})
+}) ;
 
 // time routr
 app.get('/time', (request, response) => {
     const date = new Date() ;
     const time = `${date.getHours()}:${date.getMinutes()}`
     response.json({status : 200, message: time})
-})
+}) ;
+
+// hello route 
+app.get('/hello/:ID', (request, response) => {
+    const {ID} = request.params ;
+    response.json({status: 200 , message: `Hello ${ID}!` })
+}) ;
+
+// search route 
+app.get(`/search` , (request , response) => {
+    const {s} = request.query ;
+    if (s) {
+        response.json({status: 200 , message: "ok" , data: s }) ;
+    } else {
+        response.status(500).json({status: 500 , error: true, message: `you have to provide a search`})
+    }
+}) ;

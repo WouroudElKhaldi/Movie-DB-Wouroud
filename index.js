194aab3f3b09491a2cccbcf23ce8b1054b4e8a0c
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-09-21 16:18:05 +0300

    Step 3

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ app.get('/', (request , response) => {
 
 app.listen(port, () => console.log(`Server is now listening on port ${port}
 check the local host: http://localhost:${port}`))
+
+// test route
+app.get('/test' , (request, response) => {
+    response.json({status: 200 , message : "OK"})
+})
+
+// time routr
+app.get('/time', (request, response) => {
+    const date = new Date() ;
+    const time = `${date.getHours()}:${date.getMinutes()}`
+    response.json({status : 200, message: time})
+})
